2f48fc8daf40bb755e16e673751eb71ff15623c7
Lawrence Tan <lawrencet916@gmail.com>
2020-02-19 15:58:40 +1100

    Update Icon and Timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -16,6 +16,37 @@ function Timeline() {
     <div className="timeline">
       <h1>Timeline</h1>
       <VerticalTimeline>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element-work"
+          date=" Oct 2019"
+          iconStyle={{ background: "#000000", color: "#fff" }}
+          icon={<FontAwesomeIcon icon={faAward} />}
+        >
+          <h1>AWS Certified Developer - Associate</h1>
+          <h2>Amazon Web Service</h2>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element-work"
+          date=" May 2019"
+          iconStyle={{ background: "#000000", color: "#fff" }}
+          icon={<FontAwesomeIcon icon={faAward} />}
+        >
+          <h1>MCSD - App Builder</h1>
+          <h2>Microsoft Certified Solutions Developer</h2>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element-work"
+          date=" May 2019"
+          iconStyle={{ background: "#000000", color: "#fff" }}
+          icon={<FontAwesomeIcon icon={faAward} />}
+        >
+          <h1>MCSA - Web Application</h1>
+          <h2>Microsoft Certified Solutions Associate</h2>
+        </VerticalTimelineElement>
+
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date=" Sep 2017 - Present"
